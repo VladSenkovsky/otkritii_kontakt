4fb9e045f136442173ae045a7a35cce7b002b2b4
VladSenkovsky <vlad.senkovsky@gmail.com>
2021-03-30 18:27:42 +0300

    xml, without deleted tags

--- create_script.sql
+++ create_script.sql
@@ -10,11 +10,11 @@ CREATE TABLE SHOPS (
     PRIMARY KEY (id)
 );
 COMMIT;
-INSERT INTO SHOPS (name, url, opens, closes) VALUES ('shop1', 'url1', '10:00:00',  '18:00:00'); 
-INSERT INTO SHOPS (name, url, opens, closes) VALUES ('shop2', 'url2', '8:00:00',  '19:00:00'); 
-INSERT INTO SHOPS (name, url, opens, closes) VALUES ('shop3', 'url3', '11:15:00',  '20:15:00'); 
-INSERT INTO SHOPS (name, url, opens, closes) VALUES ('shop4', 'url4', '7:30:00',  '15:45:00'); 
-INSERT INTO SHOPS (name, url, opens, closes) VALUES ('shop5', 'url5', '9:00:00',  '23:00:00'); 
+INSERT INTO SHOPS (name, url, opens, closes) VALUES ('shop1', 'url1', '10:00:00',  '23:00:00');
+INSERT INTO SHOPS (name, url, opens, closes) VALUES ('shop2', 'url2', '8:00:00',  '19:00:00');
+INSERT INTO SHOPS (name, url, opens, closes) VALUES ('shop3', 'url3', '11:15:00',  '20:15:00');
+INSERT INTO SHOPS (name, url, opens, closes) VALUES ('shop4', 'url4', '7:30:00',  '15:45:00');
+INSERT INTO SHOPS (name, url, opens, closes) VALUES ('shop5', 'url5', '9:00:00',  '23:00:00');
 COMMIT;
 CREATE TABLE PRODUCTS (
     product_id INT unsigned NOT NULL AUTO_INCREMENT,
@@ -26,7 +26,7 @@ CREATE TABLE PRODUCTS (
     FOREIGN KEY (shop_id) REFERENCES SHOPS (id)
 );
 COMMIT;
-INSERT INTO PRODUCTS (shop_id, name, description, price) VALUES (1, 'prod1', 'This product was made in the <i>USA</i>.<br>At the moment we have 1100 of these items.', 1000000.00);
+INSERT INTO PRODUCTS (shop_id, name, description, price) VALUES (1, 'prod1', 'This product was made in the <i>USA</i>.<br>At the moment we have 1100 of these items.', 1000000.35);
 INSERT INTO PRODUCTS (shop_id, name, description, price) VALUES (2, 'prod2', 'Handmade product. This could be a great wedding gift. Manufacturer\'s contacts: <b>80297776655</b>.', 10.00);
 INSERT INTO PRODUCTS (shop_id, name, description, price) VALUES (3, 'prod3', 'Stock item. Discount up to <mark>70%</mark>.', 100.99);
 INSERT INTO PRODUCTS (shop_id, name, description, price) VALUES (4, 'prod4', 'Previous price of <del>120.25</del> was reduced due to new development technology.', 77.8);
@@ -39,5 +39,3 @@ INSERT INTO PRODUCTS (shop_id, name, description, price) VALUES (5, 'prod10', 'T
 INSERT INTO PRODUCTS (shop_id, name, description, price) VALUES (1, 'prod11', 'Positive reviews record holder. Produced with <sub>diogonal lamination</sub> technology', 12.35);
 INSERT INTO PRODUCTS (shop_id, name, description, price) VALUES (1, 'prod12', 'Description w/o tags', 100.10);
 INSERT INTO PRODUCTS (shop_id, name, description, price) VALUES (2, 'prod13', 'This product was made in <i>Britain</i>. At the moment we have <b>398</b> of these items.', 55);
-
-
--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <string>
 #include <iostream>
+#include <fstream>
 #include <ctime>
 #include <vector>
+#include <time.h>
+#include <iomanip>
 #include "products.h"
 #include "shops.h"
 #include "mysql/mysql.h"
@@ -13,6 +16,8 @@ MYSQL_RES *result;
 MYSQL_ROW row;
 int query_state;
 
+static int shopSize;
+static int productSize;
 // g++ main.cpp -L/usr/include/mysql -lmysqlclient
 Shop* getShopsInfo (MYSQL *connection) {
   mysql_query(connection, "SELECT COUNT(*) FROM SHOPS");
@@ -20,52 +25,39 @@ Shop* getShopsInfo (MYSQL *connection) {
   row = mysql_fetch_row(result);
   int amount = atoi(row[0]);
   Shop *array = new Shop[amount];
+  shopSize = amount;
   mysql_free_result(result);
-
-  mysql_query(connection, "SELECT id FROM SHOPS");
+  mysql_query(connection, "SELECT id FROM SHOPS order by id");
   result = mysql_store_result(connection);
   int counter = 0;
   while(row = mysql_fetch_row(result)) {
     for (int i=0 ; i < mysql_num_fields(result); i++){
-      array[counter].setID(atoi(row[i])); //Выводим все что есть в базе через цикл
+      array[counter].setID(atoi(row[i]));
     }
     counter++;
   }
   mysql_free_result(result);
-  mysql_query(connection, "SELECT name FROM SHOPS");
+  mysql_query(connection, "SELECT name FROM SHOPS order by id");
   result = mysql_store_result(connection);
   counter = 0;
   while(row = mysql_fetch_row(result)) {
     for (int i=0 ; i <mysql_num_fields(result); i++){
-      array[counter].setName(row[i]); //Выводим все что есть в базе через цикл
+      array[counter].setName(row[i]);
     }
     counter++;
   }
   mysql_free_result(result);
-  mysql_query(connection, "SELECT url FROM SHOPS");
+  mysql_query(connection, "SELECT url FROM SHOPS order by id");
   result = mysql_store_result(connection);
   counter = 0;
   while(row = mysql_fetch_row(result)) {
     for (int i=0 ; i <mysql_num_fields(result); i++){
-      array[counter].setURL(row[i]); //Выводим все что есть в базе через цикл
+      array[counter].setURL(row[i]);
     }
     counter++;
   }
   mysql_free_result(result);
-  mysql_query(connection, "SELECT opens FROM SHOPS");
-  result = mysql_store_result(connection);
-  counter = 0;
-  while(row = mysql_fetch_row(result)) {
-    for (int i=0 ; i <mysql_num_fields(result); i++){
-      struct tm tm;
-      strptime(row[i], "%H:%M:%S", &tm);
-      time_t t = mktime(&tm);
-      array[counter].setOpen(t); //Выводим все что есть в базе через цикл
-    }
-    counter++;
-  }
-  mysql_free_result(result);
-  mysql_query(connection, "SELECT closes FROM SHOPS");
+  mysql_query(connection, "SELECT opens FROM SHOPS order by id");
   result = mysql_store_result(connection);
   counter = 0;
   while(row = mysql_fetch_row(result)) {
@@ -73,7 +65,20 @@ Shop* getShopsInfo (MYSQL *connection) {
       struct tm tm;
       strptime(row[i], "%H:%M:%S", &tm);
       time_t t = mktime(&tm);
-      array[counter].setClose(t); //Выводим все что есть в базе через цикл
+      array[counter].setOpen(t);
+    }
+    counter++;
+  }
+  mysql_free_result(result);
+  mysql_query(connection, "SELECT closes FROM SHOPS order by id");
+  result = mysql_store_result(connection);
+  counter = 0;
+  while(row = mysql_fetch_row(result)) {
+    for (int i=0 ; i <mysql_num_fields(result); i++){
+      struct tm tm;
+      strptime(row[i], "%H:%M:%S", &tm);
+      time_t t = mktime(&tm);
+      array[counter].setClose(t);
     }
     counter++;
   }
@@ -85,60 +90,125 @@ Product* getProductsInfo(MYSQL *connection) {
   row = mysql_fetch_row(result);
   int amount = atoi(row[0]);
   Product *array = new Product[amount];
+  productSize = amount;
   mysql_free_result(result);
-
-  mysql_query(connection, "SELECT product_id FROM PRODUCTS");
+  mysql_query(connection, "SELECT product_id FROM PRODUCTS order by product_id");
   result = mysql_store_result(connection);
   int counter = 0;
   while(row = mysql_fetch_row(result)) {
     for (int i=0 ; i < mysql_num_fields(result); i++){
-      array[counter].setProductID(atoi(row[i])); //Выводим все что есть в базе через цикл
+      array[counter].setProductID(atoi(row[i]));
     }
     counter++;
   }
   mysql_free_result(result);
-  mysql_query(connection, "SELECT shop_id FROM PRODUCTS");
+  mysql_query(connection, "SELECT shop_id FROM PRODUCTS order by product_id");
   result = mysql_store_result(connection);
   counter = 0;
   while(row = mysql_fetch_row(result)) {
     for (int i=0 ; i <mysql_num_fields(result); i++){
-      array[counter].setShopId(atoi(row[i])); //Выводим все что есть в базе через цикл
+      array[counter].setShopId(atoi(row[i]));
     }
     counter++;
   }
   mysql_free_result(result);
-  mysql_query(connection, "SELECT name FROM PRODUCTS");
+  mysql_query(connection, "SELECT name FROM PRODUCTS order by product_id");
   result = mysql_store_result(connection);
   counter = 0;
   while(row = mysql_fetch_row(result)) {
     for (int i=0 ; i <mysql_num_fields(result); i++){
-      array[counter].setName(row[i]); //Выводим все что есть в базе через цикл
+      array[counter].setName(row[i]);
     }
     counter++;
   }
   mysql_free_result(result);
-  mysql_query(connection, "SELECT description FROM PRODUCTS");
+  mysql_query(connection, "SELECT description FROM PRODUCTS order by product_id");
   result = mysql_store_result(connection);
   counter = 0;
   while(row = mysql_fetch_row(result)) {
     for (int i=0 ; i <mysql_num_fields(result); i++){
-      array[counter].setDescription(row[i]); //Выводим все что есть в базе через цикл
+      array[counter].setDescription(row[i]);
     }
     counter++;
   }
   mysql_free_result(result);
-  mysql_query(connection, "SELECT price FROM PRODUCTS");
+  mysql_query(connection, "SELECT price FROM PRODUCTS order by product_id");
   result = mysql_store_result(connection);
   counter = 0;
   while(row = mysql_fetch_row(result)) {
     for (int i=0 ; i <mysql_num_fields(result); i++){
-      array[counter].setPrice(stof(row[i])); //Выводим все что есть в базе через цикл
+      array[counter].setPrice(stof(row[i]));
     }
     counter++;
   }
   return array;
 
 }
+void writeXML (std::ofstream &file, Shop *shops, Product *products) {
+    time_t t = time(0);
+    tm now = *localtime(&t);
+    now.tm_year += 1990;
+    now.tm_mon += 1;
+    tm open, close;
+  for (int i = 0; i < shopSize; i++) {
+    t = shops[i].getOpen();
+    open = *localtime(&t);
+    open.tm_year = now.tm_year;
+    open.tm_mon = now.tm_mon;
+    open.tm_mday = now.tm_mday;
+    t = shops[i].getClose();
+    close = *localtime(&t);
+    close.tm_year = now.tm_year;
+    close.tm_mon = now.tm_mon;
+    close.tm_mday = now.tm_mday;
+      if (difftime(mktime(&now), mktime(&open)) >= 0 && difftime(mktime(&close), mktime(&now)) > 0) {
+      file << "<shop id=\"" << shops[i].getID() << "\">\n";
+      file << "\t<name>" << shops[i].getName() << "</name>\n";
+      file << "\t<url>" << shops[i].getURL() << "</url>\n";
+      file << "\t<working_time>\n";
+      file << "\t\t<open>" << open.tm_hour << ":" << open.tm_min<<"</open>\n";
+      file << "\t\t<close>" << close.tm_hour << ":" << close.tm_min<<"</close>\n";
+      file << "\t<working_time>\n";
+      file << "\t<offers>\n";
+      for (int j = 0; j < productSize; j++) {
+        if (products[j].getShopID() == shops[i].getID()) {
+          file << "\t\t<item id=\"" << products[j].getProductID() << "\">\n";
+          file << "\t\t\t<name>" << products[j].getName() << "</name>\n";
+          file << "\t\t\t<description>" << products[j].getDescription() <<"</description>\n";
+          char str[64];
+          sprintf(str, "%7.2f", products[j].getPrice());
+          file << "\t\t\t<price>" << str <<"</price>\n";
+          file << "\t\t</item>\n";
+        }
+      }
+      file << "\t</offers>\n";
+     }
+    file << "</shop>\n";
+  }
+}
+// <shop id="1">
+//   <name>Мой магазин</name>
+//   <url>ololo.shop.by</url>
+//   <working_time>
+//     <open>10:00</open>
+//     <close>23:00</close>
+//   </working_time>
+//   <offers>
+//     <item id="111">
+//       <name>имя товара</name>
+//       <description>описание без html тегов</description>
+//       <price>5.80</price>
+//     </item>
+//     <item id="222">
+//     ...
+//     </item>
+//     ...
+//   </offers>
+// </shop>
+// <shop id="2">
+// ...
+// </shop>
+// ...
 int main () {
   mysql_init(&mysql);
   connection = mysql_real_connect(&mysql,"localhost","root","","shop",3306,0,0);
@@ -162,7 +232,7 @@ int main () {
   //   if (result = mysql_store_result(connection)){
   //   while(row = mysql_fetch_row(result)) {
   //   for (int i=0 ; i <mysql_num_fields(result); i++){
-  //   cout<<row[i] <<"\n"; //Выводим все что есть в базе через цикл
+  //   cout<<row[i] <<"\n";
   //   }
   //   }
   // } else {
@@ -173,14 +243,18 @@ int main () {
   Product *products = getProductsInfo(connection);
 
   char buff[20];
-  for (int i = 0; i < sizeof(shops)-3; i++) { //?????????????????????????????
+  for (int i = 0; i < shopSize; i++) {
     time_t t = shops[i].getOpen();
     strftime(buff, 20, "%H:%M:%S", localtime(&t));
     cout << buff << endl;
   }
-  for (int i = 0; i < sizeof(products); i++) { //?????????????????????????????
+  for (int i = 0; i < productSize; i++) {
     cout << products[i].getDescription() << endl;
   }
+  ofstream report;
+  report.open("report.xml");
+  writeXML(report, shops, products);
+  report.close();
   mysql_free_result(result);
   mysql_close(connection);
   return 0;
